9722e43677600582febdc54202c3e36d5b54cea6
Fr0z3Nn <luckyjohn057@gmail.com>
2021-01-20 19:03:43 +0300

    Исправлено: Исправлены маппинги ItemController

--- src/main/java/com/wbz/wbzapi/controller/ItemController.java
+++ src/main/java/com/wbz/wbzapi/controller/ItemController.java
@@ -10,32 +10,33 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/item")
 public class ItemController {
 
     private final ItemServiceImpl itemService;
 
-    @RequestMapping("/item")
+    @RequestMapping("/")
     public List<Item> getItem() {
         System.out.println("запрос прилетел");
         return itemService.getAllItems();
     }
 
-    @PostMapping("/editItem")
+    @PostMapping("/edit")
     public List<Item> editItem(@RequestBody Item item) {
         itemService.editItem(item.getId(), item.getName(), item.getDescription(), item.getPrice(), item.getImage());
         System.out.println("UPDATE " + item.getId());
         return itemService.getAllItems();
     }
 
-    @PostMapping("/addItem")
+    @PostMapping("/add")
     public List<Item> addItem(@RequestBody Item item) {
         System.out.println("ADD");
         return itemService.addItem(item.getName(), item.getDescription(), item.getPrice(), item.getImage());
     }
-    @PostMapping("/deleteItem")
-    public List<Item> deleteItem(@RequestBody Item item) {
+    @PostMapping("/delete/{id}")
+    public List<Item> deleteItem(@PathVariable long id) {
         System.out.println("ОПАЧА УДАЛЯЕМ");
-         itemService.deleteItem(item.getId());
+        itemService.deleteItem(id);
          return itemService.getAllItems();
     }
 }
